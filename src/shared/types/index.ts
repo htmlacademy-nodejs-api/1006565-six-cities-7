ef99a984f91bd0edb146150b43cf8513ff8b1248
Natalia <glukomani@gmail.com>
2024-05-07 22:53:43 +0200

    Space

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -2,5 +2,4 @@ export { Category } from './category.type.js';
 export { OfferType } from './offer-type.enum.js';
 export { User } from './user.type.js';
 export { Offer } from './offer.type.js';
-
 export { MockServerData } from './mock-server-data.type.js';
