d67b64485b85e0f2874ed7723b599512340f805a
Natalia <glukomani@gmail.com>
2024-05-17 23:28:45 +0200

    category service

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,6 +5,8 @@ import { RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/index.js';
+import { createCategoryContainer } from './shared/modules/category/index.js';
+
 
 
 
@@ -12,6 +14,8 @@ async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
+    createCategoryContainer(),
+
   );
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
