53f17300d41a3c6f3a02a45cc031c3b5a517849e
Natalia <glukomani@gmail.com>
2024-05-17 16:40:30 +0200

    add dependencies to models

--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -1,5 +1,6 @@
 import { Container } from 'inversify';
-
+import { types } from '@typegoose/typegoose';
+import { UserEntity, UserModel } from './user.entity.js';
 import { UserService } from './user-service.interface.js';
 import { Component } from '../../types/index.js';
 import { DefaultUserService } from './default-user.service.js';
@@ -7,6 +8,7 @@ import { DefaultUserService } from './default-user.service.js';
 export function createUserContainer() {
   const userContainer = new Container();
   userContainer.bind<UserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
+  userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
   return userContainer;
 }
