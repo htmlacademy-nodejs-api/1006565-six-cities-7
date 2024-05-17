c71a06fac69015a3c721271982c72357d7ff2c6b
Natalia <glukomani@gmail.com>
2024-05-17 15:20:32 +0200

    User model update

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -11,15 +11,31 @@ const userSchema = new Schema({
     type: String,
     unique: true,
     required: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+
   },
-  avatarPath: String,
+  avatarPath: {
+    type: String,
+    minlength: [5, 'Min length for avatar path is 5'],
+    match: [/^(https?:\/\/.*\.(?:png|jpg))$/, 'Avatar has incorrect format'],
+  },
   name: {
     type: String,
-    minlength: 1,
-    maxlength: 15
+    required: true,
+    minlength: [1, 'Min length for name is 1'],
+    maxlength: [15, 'Max length for name is 15'],
   },
-  type: String,
-  password: String
+  type: {
+    type: String,
+    required: true,
+    enum: ['pro', 'basic']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Min length for password is 6'],
+    maxlength: [12, 'Max length for password is 12'],
+  },
 }, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
