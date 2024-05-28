24baad47513ed1dcb04d3366b4d7e3a14a4920f4
Natalia <glukomani@gmail.com>
2024-05-28 14:49:49 +0200

    Comment service update

--- src/shared/modules/comment/comment-service.interface.ts
+++ src/shared/modules/comment/comment-service.interface.ts
@@ -5,6 +5,7 @@ import { CreateCommentDto } from './dto/create-comment.dto.js';
 
 export interface CommentService {
   create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>>;
-  findByText(email: string): Promise<DocumentType<CommentEntity> | null>;
-  findOrCreate(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>>;
+  findByText(text: string): Promise<DocumentType<CommentEntity> | null>;
+  deleteByOfferId(offerId: string): Promise<number>;
+  findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]>;
 }
